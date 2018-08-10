7212c445509c8e23e15923fdbdf0c479cb15bfd1
Alok Kumar <rajalokan@gmail.com>
2018-08-10 08:57:05 +0530

    Working autorun

--- autorun.sh
+++ autorun.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 is_sclib_imported 2> /dev/null ||
-    SCLIB_PATH="/tmp/sclib.sh"
+    SCLIB_PATH="/tmp/okanstack/sclib.sh"
     SCLIB_GIT_RAW_URL="https://raw.githubusercontent.com/rajalokan/cloud-installer/master/scripts/sclib.sh"
     if [[ ! -f ${SCLIB_PATH} ]]; then
         wget ${SCLIB_GIT_RAW_URL} -O ${SCLIB_PATH}
@@ -10,14 +10,15 @@ is_sclib_imported 2> /dev/null ||
 
 info_block "Preconfiguring Bootstraping dotfiles"
 
-dotfiles_temp_path="/tmp/okanstack/"
-dotfiles_repo="https://github.com/rajalokan/okanstack"
+dotfiles_temp_path="/tmp/okanstack/ansible_roles/dotfiles"
+dotfiles_repo="https://github.com/rajalokan/ansible-role-dotfiles"
 if [[ ! -d ${dotfiles_temp_path} ]]; then
     git clone ${dotfiles_repo} ${dotfiles_temp_path}
 fi
 
 # Install ansible
 is_package_installed git || install_package git
+is_package_installed ansible || info_block "ansible not installed. Exiting"
 
 pushd ${dotfiles_temp_path}/ansible/ >/dev/null
     ansible-playbook -i "localhost," -c local playbook.yaml
